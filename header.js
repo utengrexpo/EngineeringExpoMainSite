5a0c7d83bf5411d9f6d338826f7e50aa8f1ccafb
umop-ap1sdn <cmdevon2003@gmail.com>
2023-04-17 12:24:12 -0500

    Change all file paths on homepage

--- header.js
+++ header.js
@@ -2,15 +2,15 @@ document.write('\
 \<div class="topnav" id="theTopnav">    \
 <div class="topnav_bg"></div>           \
 <a href="/" class="active">Home</a>     \
-<a href="/corporate">Corporate</a>      \
-<a href="/student">Student</a>          \
-<a href="/student/resources">Resources</a>  \
-<a href="/corporate/schedule">Schedule</a>  \
-<a href="/student/senior-reception">Senior Reception</a>    \
-<a href="/project">Expo Service Project (New!)</a>          \
-<a href="/faq">FAQ</a>                  \
-<a href="/about">About Us</a>           \
-<a href="/additional-links">Additional Links</a>            \
+<a href="corporate">Corporate</a>      \
+<a href="student">Student</a>          \
+<a href="student/resources">Resources</a>  \
+<a href="corporate/schedule">Schedule</a>  \
+<a href="student/senior-reception">Senior Reception</a>    \
+<a href="project">Expo Service Project (New!)</a>          \
+<a href="faq">FAQ</a>                  \
+<a href="about">About Us</a>           \
+<a href="additional-links">Additional Links</a>            \
 <a href="javascript:void(0);" class="icon" onclick="myFunction()">  \
     <i class="fa fa-bars"></i>          \
 </a>                                    \
