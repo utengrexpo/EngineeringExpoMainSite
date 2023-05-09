1989c522451b240ccb4bbfeaa186db81f35637b2
umop-ap1sdn <cmdevon2003@gmail.com>
2023-05-09 18:35:11 -0500

    Begin Updating About Us

---
+++ about/vars.js
@@ -0,0 +1,15 @@
+const path = "/images2023/committee/";
+const majorPlacehold = "Major, Year"
+const linkedInPlacehold = "https://www.linkedin.com"
+
+const names = [["Brandon Matthews", "Audrey Leyva"], 
+["Sourish Wawdhane (Chair)", "Emma George", "Caleb Devon", "Regina Ramirez-Anzures", "Gabby Smith", "Tracy Miranda"],
+["Minh Quan Duong (Chair)", "Uma Paul", "Laura Murphey", "Cindey Xiao"],
+["Lorren Banker (Chair)", "Jenna May", "Taima Crean", "Evan O'Suilleabhain", "Manisha Sakthivelnathan"],
+["Falak Shah (Chair)", "Phoenix Saavedra", "Basil Al Rehani", "Yashna Singhania", "Tanisha Bhattacharya", "Bhanaviya Venkat"]];
+
+const files = [["Brandon_Matthews.webp", "Audrey_Leyva.webp"], 
+["Sourish_Wawdhane.webp", "Emma_George.webp", "Caleb_Devon.webp", "Regina_Ramirez.webp", "Gabby_Smith.webp", "Tracy_Miranda.webp"],
+["Minh_Quan_Duong.webp", "Uma_Paul.webp", "Laura_Murphey.webp", "Cindey_Xiao.webp"],
+["Lorren_Banker.webp", "Jenna_May.webp", "Taima_Crean.webp", "Evan_OSuilleabhain.webp", "Manisha_Sakthivelnathan.webp"],
+["Falak_Shah.webp", "Phoenix_Saavedra.webp", "Basil_Rehani.webp", "Yashna_Singhania.webp", "Tanisha_Bhattacharya.jfif", "Bhanaviya_Venkat.webp"]];
