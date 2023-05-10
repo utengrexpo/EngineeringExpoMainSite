43360e615509e21c6595dc0dfc7646d11b015318
umop-ap1sdn <cmdevon2003@gmail.com>
2023-05-09 19:13:00 -0500

    Added LinkedIns

--- about/vars.js
+++ about/vars.js
@@ -4,7 +4,7 @@ const linkedInPlacehold = "https://www.linkedin.com"
 
 const names = [["Brandon Matthews", "Audrey Leyva"], 
 ["Sourish Wawdhane (Chair)", "Emma George", "Caleb Devon", "Regina Ramirez-Anzures", "Gabby Smith", "Tracy Miranda"],
-["Minh Quan Duong (Chair)", "Uma Paul", "Laura Murphey", "Cindey Xiao"],
+["Minh Quan Duong (Chair)", "Uma Paul", "Laura Murphy", "Cindey Xiao"],
 ["Lorren Banker (Chair)", "Jenna May", "Taima Crean", "Evan O'Suilleabhain", "Manisha Sakthivelnathan"],
 ["Falak Shah (Chair)", "Phoenix Saavedra", "Basil Al Rehani", "Yashna Singhania", "Tanisha Bhattacharya", "Bhanaviya Venkat"]];
 
@@ -13,3 +13,9 @@ const files = [["Brandon_Matthews.webp", "Audrey_Leyva.webp"],
 ["Minh_Quan_Duong.webp", "Uma_Paul.webp", "Laura_Murphey.webp", "Cindey_Xiao.webp"],
 ["Lorren_Banker.webp", "Jenna_May.webp", "Taima_Crean.webp", "Evan_OSuilleabhain.webp", "Manisha_Sakthivelnathan.webp"],
 ["Falak_Shah.webp", "Phoenix_Saavedra.webp", "Basil_Rehani.webp", "Yashna_Singhania.webp", "Tanisha_Bhattacharya.jfif", "Bhanaviya_Venkat.webp"]];
+
+const linkedIn = [["https://www.linkedin.com/in/brandoncmatthews/", "https://www.linkedin.com/in/audrey-leyva/"], 
+["https://www.linkedin.com/in/sourishw/", "https://www.linkedin.com/in/emmagracegeorge/", "https://www.linkedin.com/in/caleb-devon-604a41227/", "https://www.linkedin.com/in/regina-ramirez-anzures-8a50b0270/", "https://www.linkedin.com/in/smithgmca/", "https://www.linkedin.com/in/tracyym/"],
+["https://www.linkedin.com/in/minh-quan-duong-046aa51bb/", "https://www.linkedin.com/in/uma-paul-b48a7425b/", "https://www.linkedin.com/in/laura-murphy-12626024b/", "https://www.linkedin.com/in/cindey-xiao-969a321a7/"],
+["https://www.linkedin.com/in/lorren-banker-0a55691b0/", "https://www.linkedin.com/in/jenna-may-836b0a21b/", "https://www.linkedin.com/in/taima00/", "https://www.linkedin.com/in/evan-osuilleabhain/", "https://www.linkedin.com/in/manisha-sakthivelnathan/"],
+["https://www.linkedin.com/in/falak-shah-735259205/", "https://www.linkedin.com/in/phoenixsaavedra/", "https://www.linkedin.com/in/basil-rehani-80666224a/", "https://www.linkedin.com/in/yashna-singhania-304522211/", "https://www.linkedin.com/in/tanisha-bhattacharya-1783851a9/", "https://www.linkedin.com/in/bhanaviya-venkat-307520219/"]]
