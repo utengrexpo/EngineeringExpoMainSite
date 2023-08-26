8b338e6a0d412ce857c2fdc15af52e137c7f1e2c
SourishW <sourishw@gmail.com>
2023-08-25 22:45:34 -0500

    Added the final few booths

--- boothmaps/index.js
+++ boothmaps/index.js
@@ -1,4 +1,3 @@
-
 // scroll to center on load
 window.addEventListener("load", function () {
   let container = document.getElementById("scrollableMapContainer");
@@ -9,6 +8,29 @@ window.addEventListener("load", function () {
   // Scroll the container to the center
   container.scrollTop = centerY;
   container.scrollLeft = centerX;
+});
+
+window.addEventListener("load", function () {
+  const mapElement = document.querySelector('map[name="image-map"]');
+  fetch("/boothmaps/boothCoordinates.json")
+    .then((response) => response.json())
+    .then((data) => {
+      for (const boothNumber in data) {
+        if (data.hasOwnProperty(boothNumber)) {
+          const coordinates = data[boothNumber];
+          const newArea = document.createElement("area");
+          newArea.setAttribute("shape", "rect");
+          newArea.setAttribute("coords", coordinates);
+          newArea.setAttribute("href", `${boothNumber}`);
+          newArea.setAttribute("alt", `Booth #${boothNumber}`);
+
+          mapElement.appendChild(newArea);
+        }
+      }
+    })
+    .catch((error) => {
+      console.error("Error reading in booth coordinates:", error);
+    });
 });
 
 const slider = document.getElementById("myZoom");
