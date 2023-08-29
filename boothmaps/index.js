210440a00f0a5f98ac2e4adbf8d3c89481c9830e
SourishW <sourishw@gmail.com>
2023-08-28 19:19:02 -0500

    Clicking on a map redirects properly!!

--- boothmaps/index.js
+++ boothmaps/index.js
@@ -3,8 +3,8 @@ const imageWidth = mapImage.naturalWidth;
 const imageHeight = mapImage.naturalHeight;
 const imageCenterX = imageWidth / 2;
 const imageCenterY = imageHeight / 2;
-const highlight1 = document.getElementById("highlight1");
-const highlight2 = document.getElementById("highlight2");
+const highlight1 = document.getElementById("highlight1"); // the one that the user will manipulate
+const highlight2 = document.getElementById("highlight2"); // the one that will be placed by the dropdown
 
 function scrollToCenter() {
   const container = document.getElementById("scrollableMapContainer");
@@ -18,10 +18,6 @@ function scrollToCenter() {
   document.documentElement.style.setProperty("--map-zoom", 0.2);
 }
 
-// function scrollIntoView(element){
-//   element.scrollIntoView({ behavior: "smooth"})
-// }
-
 // scroll to center on load
 window.addEventListener("load", function () {
   scrollToCenter();
@@ -30,8 +26,8 @@ window.addEventListener("load", function () {
 function updateAreaAttributes(newArea, coordinates, boothNumber) {
   newArea.setAttribute("shape", "rect");
   newArea.setAttribute("coords", coordinates);
-  newArea.setAttribute("href", `${boothNumber}`);
   newArea.setAttribute("alt", `Booth #${boothNumber}`);
+  newArea.setAttribute("boothNumber", boothNumber);
 }
 
 function alignHiglightToArea(areaTargeting, highlightAligning) {
@@ -55,6 +51,10 @@ function alignHiglightToArea(areaTargeting, highlightAligning) {
   highlightAligning.style.left = updatedOffsetHorizontal + "px";
   highlightAligning.style.top = updatedOffsetVertical + "px";
   highlightAligning.style.display = "block";
+  highlightAligning.setAttribute(
+    "boothNumber",
+    areaTargeting.getAttribute("boothNumber")
+  );
 }
 
 function addBoothHoverListeners(newArea) {
@@ -63,9 +63,11 @@ function addBoothHoverListeners(newArea) {
   });
 
   newArea.addEventListener("mouseleave", function () {
-    // highlight.style.display = "none";
+    // highlight1.style.display = "none";
   });
 }
+
+let boothNumberToAreaDict = new Object();
 
 // pull in location info for all of the booths
 window.addEventListener("load", function () {
@@ -83,6 +85,7 @@ window.addEventListener("load", function () {
           updateAreaAttributes(newArea, coordinates, boothNumber);
           addBoothHoverListeners(newArea);
           mapElement.appendChild(newArea);
+          boothNumberToAreaDict[boothNumber] = newArea;
         }
       }
     })
@@ -90,11 +93,18 @@ window.addEventListener("load", function () {
     .catch((error) => {
       console.error("Error reading in booth coordinates:", error);
     });
+
+  document
+    .getElementById("scrollableMapContainer")
+    .addEventListener("mouseleave", function () {
+      highlight1.style.display = "none";
+    });
 });
 
 const slider = document.getElementById("myZoom");
 slider.addEventListener("input", function () {
   highlight1.style.display = "none";
+  highlight2.style.display = "none";
   const sliderValue = this.value;
   document.documentElement.style.setProperty("--map-zoom", sliderValue);
 });
@@ -125,6 +135,8 @@ async function populateEmployerData(whichDay) {
   }
 }
 
+const boothNumberToEmployerName = new Object();
+
 function updateDay(whichDay) {
   populateEmployerData(whichDay).then(() => {
     // update dropdown label
@@ -136,24 +148,39 @@ function updateDay(whichDay) {
     const employerSelectionDropdown = document.getElementById(
       "employerSelectionDropdown"
     );
+
+    // create default to start with
     employerSelectionDropdown.innerHTML = "";
+    instruction = document.createElement("option");
+    instruction.innerHTML = "Select an employer";
+    employerSelectionDropdown.appendChild(instruction);
 
     // update dropdown values
     const employerDataRelevant =
       whichDay == 1 ? day1EmployerData : day2EmployerData;
-    console.log(employerDataRelevant);
 
     for (const employerName in employerDataRelevant) {
       if (!employerDataRelevant.hasOwnProperty(employerName)) {
         continue;
       }
+
+      // add employer to list of options
       const newEmployerOption = document.createElement("option");
       newEmployerOption.setAttribute("value", employerName);
       newEmployerOption.innerHTML = employerName;
       employerSelectionDropdown.appendChild(newEmployerOption);
+
+      // create a mapping from booth numbers to employers
+      for (const boothNumber of employerDataRelevant[employerName][
+        `Day ${whichDay} Booth`
+      ]) {
+        boothNumberToEmployerName[boothNumber] = employerName;
+      }
     }
 
     // clear fields for the selected employer
+    const employerInfoDiv = document.getElementById("employerInformationDiv");
+    employerInfoDiv.style.display = "none";
   });
 }
 
@@ -168,4 +195,73 @@ window.addEventListener("load", function () {
       }
     });
   });
+});
+
+const employerSelectionDropdown = document.getElementById(
+  "employerSelectionDropdown"
+);
+
+function updateEmployerDataBasedOnDropdownSelected() {
+  // find which employer was selected
+  const selectedEmployer = employerSelectionDropdown.value;
+  const relevantEmployerData = day1EmployerData[selectedEmployer];
+
+  // find the booth number
+  const correctBoothRelatedKey =
+    "Day 1 Booth" in relevantEmployerData ? "Day 1 Booth" : "Day 2 Booth";
+  const boothNumberData = relevantEmployerData[correctBoothRelatedKey];
+  const boothNumber = boothNumberData[0];
+
+  // put the highlight on the booth, and scroll highlight into view
+  alignHiglightToArea(boothNumberToAreaDict[boothNumber], highlight2);
+  highlight2.scrollIntoView({
+    behavior: "auto",
+    block: "center",
+    inline: "center",
+    behavior: "smooth",
+  });
+
+  // update employer information
+  const employerInfoDiv = document.getElementById("employerInformationDiv");
+  employerInfoDiv.style.display = "inline-block";
+  const thingsWeNeedToUpdate = {
+    companyName: "Organization Name",
+    boothNumbers: correctBoothRelatedKey,
+    companyIndustry: "Industry",
+    companyMajorsRecruited: "Majors Recruited",
+    companyPositionTypesRecruited: "Position Types",
+    companyDegreeLevelsRecruited: "Degree Levels Recruited",
+    companySiteLink: "Website",
+  };
+  for (const [documentElementID, keyInEmployerData] of Object.entries(
+    thingsWeNeedToUpdate
+  )) {
+    // need to replace "," with ", " so it looks better
+    document.getElementById(documentElementID).innerHTML = String(
+      relevantEmployerData[keyInEmployerData]
+    ).replaceAll(",", ", ");
+  }
+  const companySiteLink = document.getElementById("companySiteLink");
+  companySiteLink.setAttribute("href", relevantEmployerData["Website"]);
+  companySiteLink.setAttribute("target", "_blank");
+}
+
+employerSelectionDropdown.addEventListener(
+  "input",
+  updateEmployerDataBasedOnDropdownSelected
+);
+
+highlight1.addEventListener("click", function () {
+  const boothNumber = highlight1.getAttribute("boothNumber");
+  const employerName = boothNumberToEmployerName[boothNumber];
+
+  // select from dropdown, so we can synchronize changes
+  const employerOptionToSelect = Array.from(
+    employerSelectionDropdown.options
+  ).find((option) => option.value == employerName);
+  if (!employerOptionToSelect) {
+    console.error("unable to find the employer for the area selected");
+  }
+  employerOptionToSelect.selected = true;
+  updateEmployerDataBasedOnDropdownSelected();
 });
