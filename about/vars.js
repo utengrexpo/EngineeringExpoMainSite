6b74718656be8903797841a37e3fc0f6c1dce32f
Caleb Devon <cmdevon2003@gmail.com>
2024-05-07 21:25:54 -0500

    finish updating about page

--- about/vars.js
+++ about/vars.js
@@ -3,25 +3,25 @@ const majorPlacehold = "Major, Year"
 const linkedInPlacehold = "https://www.linkedin.com"
 
 const names = [["Sourish Wawdhane", "Minh Quan Duong"], 
-["Caleb Devon (Chair)", "Vivek Keval", "Callum Murphey", "Anik Patel", "Collis Whitfield"],
+["Caleb Devon (Chair)", "Vivek Keval", "Callum Murphy", "Anik Patel", "Collis Whitfield"],
 ["Jett Winkler (Chair)", "Kayla Awadin", "Jiwoo Park", "Carlos Piña-Martinez", "Ella Tomaselli"],
 ["Lorren Banker (Chair)", "Michelle Alex", "Leona Alonso", "Lisa Du", "Bhanaviya Venkat", "Alejandro Villasenor"],
 ["Manisha Sakthivelnathan (Chair)", "Joseph Jimenez", "Gabriela Martin", "Evan O’Suilleabhain", "Connor Sanders", "Jennifer Xue"]];
 
 const files = [["Sourish_Wawdhane.JPG", "Minh_Duong.JPG"], 
-["Caleb_Devon.JPG", "Vivek_Keval.JPG", "Callum_Murphey.jpg", "Anik_Patel.JPG", "Collis_Whitfield.JPG"],
+["Caleb_Devon.JPG", "Vivek_Keval.JPG", "Callum_Murphy.jpg", "Anik_Patel.JPG", "Collis_Whitfield.JPG"],
 ["Jett_Winkler.JPG", "Kayla_Awadin.JPG", "Jiwoo_Park.JPG", "Carlos_Pina.JPG", "Ella_Tomaselli.JPG"],
-["Lorren_Banker.JPG", "Michelle_Alex.JPG", "Jiwoo_Park.JPG", "Lisa_Du.JPG", "Bhanaviya_Venkat.JPG", "Alejandro_Villasenor.JPG"],
+["Lorren_Banker.JPG", "Michelle_Alex.JPG", "Leona_Alonso.JPG", "Lisa_Du.JPG", "Bhanaviya_Venkat.JPG", "Alejandro_Villasenor.JPG"],
 ["Manisha_Sakthivelnathan.JPG", "Joseph_Jiminez.JPG", "Gabriela_Martin.JPG", "Evan_OSuilleabhain.JPG", "Connor_Sanders.JPG", "Jennifer_Xue.JPG"]];
 
-const linkedIn = [["https://www.linkedin.com/in/brandoncmatthews/", "https://www.linkedin.com/in/audrey-leyva/"], 
-["https://www.linkedin.com/in/sourishw/", "https://www.linkedin.com/in/emmagracegeorge/", "https://www.linkedin.com/in/caleb-devon-604a41227/", "https://www.linkedin.com/in/regina-ramirez-anzures-8a50b0270/", "https://www.linkedin.com/in/smithgmca/", "https://www.linkedin.com/in/tracyym/"],
-["https://www.linkedin.com/in/minh-quan-duong-046aa51bb/", "https://www.linkedin.com/in/uma-paul-b48a7425b/", "https://www.linkedin.com/in/laura-murphy-12626024b/", "https://www.linkedin.com/in/cindey-xiao-969a321a7/"],
-["https://www.linkedin.com/in/lorren-banker-0a55691b0/", "https://www.linkedin.com/in/jenna-may-836b0a21b/", "https://www.linkedin.com/in/taima00/", "https://www.linkedin.com/in/evan-osuilleabhain/", "https://www.linkedin.com/in/manisha-sakthivelnathan/"],
-["https://www.linkedin.com/in/falak-shah-735259205/", "https://www.linkedin.com/in/phoenixsaavedra/", "https://www.linkedin.com/in/basil-rehani-80666224a/", "https://www.linkedin.com/in/yashna-singhania-304522211/", "https://www.linkedin.com/in/tanisha-bhattacharya-1783851a9/", "https://www.linkedin.com/in/bhanaviya-venkat-307520219/"]]
+const linkedIn = [["https://www.linkedin.com/in/sourishw/", "https://www.linkedin.com/in/minh-quan-duong-046aa51bb/"], 
+["https://www.linkedin.com/in/caleb-devon/", "https://www.linkedin.com/in/vivek-keval/", "https://www.linkedin.com/in/callum-s-murphy/", "https://www.linkedin.com/in/anik-patel-800a8322a/", "https://www.linkedin.com/in/collis-whitfield-40b3801a3/"],
+["https://www.linkedin.com/in/jett-winkler-62a114264/", "https://www.linkedin.com/in/kaylaawadin/", "https://www.linkedin.com/in/jiwoooop/", "https://www.linkedin.com/in/carlos-pi%C3%B1a-martinez-2724a7263/", "https://www.linkedin.com/in/ellatomaselli/"],
+["https://www.linkedin.com/in/lorren-banker-0a55691b0/", "https://www.linkedin.com/in/michelle-alex/", "https://www.linkedin.com/in/leona-alonso-701a3a204/", "https://www.linkedin.com/in/lisa-victoria-du-949999291/", "https://www.linkedin.com/in/bhanaviya-venkat-307520219/", "https://www.linkedin.com/in/alejandro-villasenor-graves-455910280/"],
+["https://www.linkedin.com/in/manisha-sakthivelnathan/", "https://www.linkedin.com/in/joseph-jimenez05/", "https://www.linkedin.com/in/gabriela-a-martin-b0772929a/", "https://www.linkedin.com/in/evan-osuilleabhain/", "https://www.linkedin.com/in/connor-sanders-18a8532b1/", "https://www.linkedin.com/in/jenniferxue333/"]]
 
-const major = [["Chemical Engineering '24", "Biomedical Engineering '24"],
-["Electrical and Computer Engineering '25", "Civil Engineering '24", "Electrical and Computer Engineering '25", "Environmental Engineering '25", "Chemical Engineering '24", "Environmental Engineering '26"],
-["Chemical Engineering '26", "Biomedical Engineering '26", "Biomedical Engineering '25", "Electrical and Computer Engineering '26"],
-["Mechanical Engineering '25", "Electrical and Computer Engineering '25", "Biomedical Engineering '25", "Mechanical Engineering '25", "Electrical and Computer Engineering '26"],
-["Mechanical Engineering '24", "Mechanical Engineering '26", "Biomedical Engineering '26", "Environmental Engineering '25", "Electrical and Computer Engineering '26", "Biomedical Engineering '26"]]
+const major = [["Electrical and Computer Engineering '25", "Chemical Engineering '26"],
+["Electrical and Computer Engineering '25", "Mechanical Engineering '27", "Electrical and Computer Engineering '25", "Electrical and Computer Engineering '26", "Civil Engineering '26"],
+["Electrical and Computer Engineering '26", "Chemical Engineering '26", "Electrical and Computer Engineering '26", "Chemical Engineering '26", "Civil Engineering '26"],
+["Mechanical Engineering '25", "Architectural Engineering '27", "Civil Engineering '27", "Biomedical Engineering '27", "Biomedical Engineering '26", "Electrical and Computer Engineering '26"],
+["Electrical and Computer Engineering '26", "Electrical and Computer Engineering '27", "Civil Engineering '27", "Mechanical Engineering '25", "Electrical and Computer Engineering '26", "Mechanical Engineering '27"]]
